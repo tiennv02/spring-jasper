f77a6be5bb6c80106a8ebed401c4ce82a9304563
tiennv02 <tiennv02@gmail.com>
2018-11-03 01:39:22 +0700

    commit build jar

--- jsp-start/src/main/java/com/report/controller/Report2.java
+++ jsp-start/src/main/java/com/report/controller/Report2.java
@@ -38,8 +38,13 @@ public class Report2 {
         }
 
         HashMap<String, Object> params = new HashMap<String, Object>();
+        params.put("p_ca", cbbCa);
         params.put("p_startDate", startDate);
         params.put("p_endDate", endDate);
+        params.put("p_can", dtNgay);
+
+        params.put("p_tenCanTV", dtNgay);
+        params.put("p_title", dtNgay);
 
         downloadService.download(ExporterService.EXTENSION_TYPE_PDF, ExporterService.TEMPLATE_2, params, response);
     }
