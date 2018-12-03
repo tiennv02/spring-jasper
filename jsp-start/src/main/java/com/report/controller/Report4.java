ed5f49d9f37fddc67c15e7549e61e9542efda987
tiennv02 <tiennv02>
2018-12-04 02:40:02 +0700

    fixbug report 2-4

--- jsp-start/src/main/java/com/report/controller/Report4.java
+++ jsp-start/src/main/java/com/report/controller/Report4.java
@@ -40,6 +40,8 @@ public class Report4 {
         HashMap<String, Object> params = new HashMap<String, Object>();
         params.put("p_startDate", startDate);
         params.put("p_endDate", endDate);
+        params.put("p_ca", cbbCa);
+        params.put("p_Date", dtNgay);
 
         downloadService.download(ExporterService.EXTENSION_TYPE_PDF, ExporterService.TEMPLATE_4, params, response);
     }
